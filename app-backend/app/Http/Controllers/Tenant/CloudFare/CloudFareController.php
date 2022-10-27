1b8c8c059096b86180a083d07d4c5a082fb627a3
Emanuel Santamaría <ecotics.esantamaria@gmail.com>
2022-10-27 09:08:41 -0500

    Actualizar dist

---
+++ app-backend/app/Http/Controllers/Tenant/CloudFare/CloudFareController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Tenant\RequestDomain;
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class CloudFareController extends Controller
+{
+}
