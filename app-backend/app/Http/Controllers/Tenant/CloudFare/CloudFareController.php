3484080d59773f352e72d9c1f5eaf4a92b763bfd
Emanuel Santamaría <ecotics.esantamaria@gmail.com>
2022-10-27 12:12:36 -0500

    Cambio en migracion

--- app-backend/app/Http/Controllers/Tenant/CloudFare/CloudFareController.php
+++ app-backend/app/Http/Controllers/Tenant/CloudFare/CloudFareController.php
@@ -8,4 +8,17 @@ use Illuminate\Http\Request;
 
 class CloudFareController extends Controller
 {
+    protected $url = 'https://api.cloudflare.com/client/v4/';
+
+    public function methodCredentials()
+    {
+        /* $response = Http::withOptions([
+            'verify' => false,
+        ])->post($this->url . '/api/auth/login', [
+            'email' => $objSetting->email,
+            'password' => $objSetting->password,
+        ]);
+
+        return json_decode((string) $response->body(), true)['access_token'];*/
+    }
 }
