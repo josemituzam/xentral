b6ba2487e37bfa1b554bf199d7e4422720dc2dcc
Emanuel Santamaría <ecotics.esantamaria@gmail.com>
2022-11-09 10:25:34 -0500

    Actualizacion en el Envioronment

--- app-frontend-landlord/src/environments/environment.ts
+++ app-frontend-landlord/src/environments/environment.ts
@@ -6,10 +6,7 @@ const baseUrl = parsedUrl.hostname
 export const environment = {
   production: false,
   hmr: false,
-  //apiUrl: `http://${baseUrl}:8080/api/v1/admin` //Developer mode;
-  apiUrl: `https://api.ispxentral.com/api/v1/admin`  //Production mode;
-
-  // apiUrl: `https://api.ispxentral.com/api/v1` //Qa mode;
+  apiUrl: `https://api.ispxentral.com/api/v1/admin`
 };
 
 /*
